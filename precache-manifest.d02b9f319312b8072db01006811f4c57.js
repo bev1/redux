735b6b2df599e6b57e070520bb0cfd2903279d8f
bev1 <longer30001@gmail.com>
2020-05-19 23:09:40 +0300

    Updates

---
+++ precache-manifest.d02b9f319312b8072db01006811f4c57.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "264000d66b88773c1fdae8a074c075d5",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "9ba778b8031ff8ba8617",
+    "url": "/test/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1939eac2917fc49dd574",
+    "url": "/test/static/css/main.75eff5c2.chunk.css"
+  },
+  {
+    "revision": "9ba778b8031ff8ba8617",
+    "url": "/test/static/js/2.11a2204f.chunk.js"
+  },
+  {
+    "revision": "edbc79938c564134066dc08722bf82e3",
+    "url": "/test/static/js/2.11a2204f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1939eac2917fc49dd574",
+    "url": "/test/static/js/main.4065a26c.chunk.js"
+  },
+  {
+    "revision": "f63ecef71b330c334131",
+    "url": "/test/static/js/runtime-main.f77b3668.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
